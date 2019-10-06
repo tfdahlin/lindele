4fff23c90395fd743e084a8963a1debb25ddf1f1
Taylor Fox Dahlin <9667869+tfdahlin@users.noreply.github.com>
2019-10-05 20:58:12 -0400

    Added some logging to the restart/reboot functionality.

--- api/util/routes.py
+++ api/util/routes.py
@@ -1,4 +1,4 @@
-import threading
+import threading, logging
 
 from wakeonlan import send_magic_packet
 
@@ -9,9 +9,13 @@ from users.util import get_user_from_request, is_logged_in
 from util.decorators import requires_params, requires_login
 from util.util import BaseHandler, reboot_machine_with_delay, mount_as_needed, wake_media_server
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 class Restart(BaseHandler):
     @requires_login()
     def get(self):
+        logger.warn('Restarting server.')
         wake_media_server()
         t = threading.Thread(target=reboot_machine_with_delay)
         t.start()
@@ -20,6 +24,7 @@ class Restart(BaseHandler):
 class Remount(BaseHandler):
     @requires_login()
     def get(self):
+        logger.warn('Remounting media server.')
         wake_media_server()
         mount_as_needed()
         return self.success()
