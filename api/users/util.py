59ba3fbaae489f169d684b4443dbf8f1214ec7ae
Taylor Fox Dahlin <9667869+tfdahlin@users.noreply.github.com>
2019-11-25 17:54:33 -0500

    Removed another unnecessary import.

--- api/users/util.py
+++ api/users/util.py
@@ -13,7 +13,7 @@ from base64 import b64encode, b64decode
 # Local file imports
 from settings import hash_iterations, hash_algo
 from settings import BASE_PATH
-from users.models import User, LoginAttempt, engine
+from users.models import User, LoginAttempt
 from util.util import access_db
 
 # PIP library imports
