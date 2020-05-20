7d44b03d992c60dc5c26847de683301839a038ee
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2020-05-20 15:56:53 -0700

    Added empty test suite.

--- api/tests/test_all.py
+++ api/tests/test_all.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Filename: tests/test_all.py
+"""Test suite file."""
+
+# Native python imports
+from unittest import TestCase
+
+# Pip library imports
+
+# Local imports
+
+class TestMusic(TestCase):
+	"""Test suite for music objects."""
+
+	def test_sample(self):
+		"""Always pass."""
+		pass
+
+class TestUsers(TestCase):
+	"""Test suite for user objects."""
+
+	def test_sample(self):
+		"""Always pass."""
+		# TODO: get rid of this eventually.
+		pass
+
+class TestUtil(TestCase):
+	"""Test suite for util functionality."""
+
+	def test_pass(self):
+		"""Always pass."""
+		# TODO: get rid of this eventually.
+		pass
