fa9df6120104e7b08c1fdd549f3460edc4ef6cb3
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2020-05-27 23:20:48 -0700

    Added RangeFileWrapper utility object for serving ranges.

--- api/util/util.py
+++ api/util/util.py
@@ -24,6 +24,44 @@ from wakeonlan import send_magic_packet
 engine = create_engine(db_uri)
 Session = sessionmaker(bind=engine)
 
+class RangeFileWrapper:
+    """FileWrapper with support for byte ranges.
+
+    Borrowed from https://gist.github.com/dcwatson/cb5d8157a8fa5a4a046e"""
+
+    def __init__(self, filelike, blksize=8192, offset=0, length=None):
+        """Initialization function for iterable wrapper."""
+        self.filelike = filelike
+        self.filelike.seek(offset, os.SEEK_SET)
+        self.remaining = length
+        self.blksize = blksize
+
+    def close(self):
+        """Closes filelike."""
+        if hasattr(self.filelike, 'close'):
+            self.filelike.close()
+
+    def __iter__(self):
+        """Returns self as iterator."""
+        return self
+
+    def next(self):
+        """Fetches next set of readable bytes."""
+        if self.remaining is None:
+            # If remaining is None, we're reading the entire file.
+            data = self.filelike.read(self.blksize)
+            if data:
+                return data
+            raise StopIteration()
+        else:
+            if self.remaining <= 0:
+                raise StopIteration()
+            data = self.filelike.read(min(self.remaining, self.blksize))
+            if not data:
+                raise StopIteration()
+            self.remaining -= len(data)
+            return data
+
 class BaseHandler(Handler):
     """Extension of pycnic's Handler class. 
     Provides utility functions for inheriting in specific handlers
