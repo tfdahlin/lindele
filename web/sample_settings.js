b366bd5c0765f1ffc72eb9feebdff6ec9ce4a534
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2020-06-08 17:25:47 -0700

    Minor change to sample_settings file.

--- web/sample_settings.js
+++ web/sample_settings.js
@@ -5,7 +5,7 @@ let settings = {
     'ssl_key_path': null,
     'ssl_cert_path': null,
     'ssl_ca_path': null,
-    'api_url': 'http://localhost',
+    'api_url': 'http://localhost:8000',
     'service_name': 'Lindele Music',
     'webPort': 80,
 }
